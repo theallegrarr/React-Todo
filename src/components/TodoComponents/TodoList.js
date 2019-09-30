e1b18190c0a9f195395e867c682c182ae1249998
Nurudeen <nurudeen.amedu@gmail.com>
2019-09-30 13:12:35 +0100

    List component completed

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,21 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React, { useState } from 'react';
+
+export default class TodoList extends React.Component {
+
+  render() {
+    return(
+      this.props.todos.map(todo => (
+        <ul>
+          <li 
+          onClick={this.props.complete.bind(this, todo.id)}
+          style={{
+            textDecoration: todo.completed ? 'line-through' : 'none'
+          }}
+          >{todo.task}</li>
+        </ul>
+      ))
+    );
+  };
+}
